69f7fd94f635e0a9cfcdd04d1c2422d8300fea16
raziescudero <inuyasha_24_4@hotmail.com>
2021-10-06 14:59:49 -0500

    Backend primeros pasos

--- src/main/java/mx/uv/App.java
+++ src/main/java/mx/uv/App.java
@@ -1,13 +1,26 @@
 package mx.uv;
 
-/**
- * Hello world!
- *
- */
+import static spark.Spark.*;
+import com.google.gson.*;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        options("/*", (request, response) -> {
+
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
     }
 }
